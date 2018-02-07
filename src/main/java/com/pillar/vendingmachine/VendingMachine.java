5b42e8fd521f2c41c2c6e276c3b8aa7f99652ddc
tbrindley <travisbrindley@gmail.com>
2018-02-06 21:29:51 -0500

    Test to determine if a coin is a penny based off its weight

--- src/main/java/com/pillar/vendingmachine/VendingMachine.java
+++ src/main/java/com/pillar/vendingmachine/VendingMachine.java
@@ -1,5 +1,7 @@
 package com.pillar.vendingmachine;
 
+import com.pillar.coins.Coin;
+
 /**
  * Created by Travis Brindley on 2/6/2018.
  */
@@ -7,5 +9,12 @@ public class VendingMachine {
 
     public String getInsertCoinLabel() {
         return "INSERT COIN";
+    }
+
+    public boolean isCoinBasedOffWeight(double weight) {
+        if(weight == Coin.getPenny().getWeight()){
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/com/pillar/VendingMachineTests.java
+++ src/test/java/com/pillar/VendingMachineTests.java
@@ -1,20 +1,28 @@
 package com.pillar;
 
+import com.pillar.coins.Coin;
 import com.pillar.vendingmachine.VendingMachine;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class VendingMachineTests {
-
+    VendingMachine vendingMachine = new VendingMachine();
     @Test
     public void vendingMachineDisplaysTextThatSaysInsertCoin() throws Exception {
-        VendingMachine vendingMachine = new VendingMachine();
+
         String expected = "INSERT COIN";
         String actual = vendingMachine.getInsertCoinLabel();
 
-       assertEquals("TestCase 1 failed.  INSERT COIN wasn't the label.",expected, actual);
+       assertEquals("Test Case 1 failed.  INSERT COIN wasn't the label.",expected, actual);
 
     }
-    
+    @Test
+    public void vendingMachineDeterminesIfCoinIsAPennyBasedOffItsWeight() throws Exception{
+        Coin penny = Coin.getPenny();
+        boolean expected = true;
+        boolean actual = vendingMachine.isCoinBasedOffWeight(penny.getWeight());
+
+        assertEquals("Test Case 2 failed.  Method didn't find the coin to be a penny", expected, actual);
+    }
 }
